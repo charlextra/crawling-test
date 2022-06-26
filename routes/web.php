c82d11249834cc0f1599f5a52f1623e663e76b6b
charlemagne_kokodoko <charlemagne.kokodoko@skilldynamics.com>
2022-06-26 13:13:01 +0400

    general updates

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\CustomLinkController::class, 'index']);
+Route::get('crawls', [App\Http\Controllers\CustomLinkController::class, 'crawls']);
+Route::post('custom_links', [App\Http\Controllers\CustomLinkController::class, 'store']);
